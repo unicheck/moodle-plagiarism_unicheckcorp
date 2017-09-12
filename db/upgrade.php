5e7eee076e8b7df345f6bbe8379d11fd39d9224f
Eropi4 <4648047+Eropi4@users.noreply.github.com>
2017-09-12 13:16:30 +0300

    Remove old db migrations (#1)
    * remove old migrations
    
    * add missing return true

--- db/upgrade.php
+++ db/upgrade.php
@@ -23,7 +23,6 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-use plagiarism_unicheck\classes\unicheck_core;
 
 if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');
@@ -47,79 +46,5 @@ require_once(dirname(__FILE__) . '/../autoloader.php');
  * @throws upgrade_exception
  */
 function xmldb_plagiarism_unicheck_upgrade($oldversion) {
-    global $DB;
-
-    $dbman = $DB->get_manager();
-
-    if ($oldversion < 2016041600) {
-        // Define field external_file_id to be added to plagiarism_unplag_files.
-        $table = new xmldb_table(UNICHECK_FILES_TABLE);
-
-        $field = new xmldb_field('external_file_id', XMLDB_TYPE_INTEGER, '10', true, null, null, null);
-        // Conditionally launch add field.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        $field = new xmldb_field('reportediturl', XMLDB_TYPE_CHAR, '255', null, null, null, null);
-        // Conditionally launch add field.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        $field = new xmldb_field('similarityscore', XMLDB_TYPE_NUMBER, '5,2', null, null, null, null, null);
-        $dbman->change_field_type($table, $field);
-
-        // Unicheck savepoint reached.
-        upgrade_plugin_savepoint(true, 2016041600, 'plagiarism', 'unicheck');
-    }
-
-    if ($oldversion < 2016100500) {
-        // Define field external_file_id to be added to plagiarism_unplag_files.
-        $table = new xmldb_table(UNICHECK_FILES_TABLE);
-
-        $field = new xmldb_field('parent_id', XMLDB_TYPE_INTEGER, '10', null, null, null, null, 'id');
-
-        // Conditionally launch add field parent_id.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        $field = new xmldb_field('type', XMLDB_TYPE_CHAR, '63', null, XMLDB_NOTNULL, null, 'document', 'filename');
-
-        // Conditionally launch add field type.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        // Unicheck savepoint reached.
-        upgrade_plugin_savepoint(true, 2016100500, 'plagiarism', 'unicheck');
-    }
-
-    if ($oldversion < 2016112200) {
-        if ($dbman->table_exists(UNICHECK_USER_DATA_TABLE)) {
-            $dbman->drop_table(UNICHECK_USER_DATA_TABLE);
-        }
-
-        $table = new xmldb_table(UNICHECK_USER_DATA_TABLE);
-
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null, null);
-        $table->add_field('user_id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, null);
-        $table->add_field('external_user_id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, null);
-        $table->add_field('external_token', XMLDB_TYPE_CHAR, '64', null, XMLDB_NOTNULL, null, null, null, null);
-
-        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'), null, null);
-        $table->add_key('user_id', XMLDB_KEY_FOREIGN, array('user_id'), 'user', 'id');
-
-        $dbman->create_table($table);
-
-        upgrade_plugin_savepoint(true, 2016112200, 'plagiarism', 'unicheck');
-    }
-
-    if ($oldversion < 2017012300) {
-        unicheck_core::migrate_users_access();
-        upgrade_plugin_savepoint(true, 2017012300, 'plagiarism', 'unicheck');
-    }
-
     return true;
 }
