a75454032e0f05811e7891be262eff7327230b85
Aleksandr Kostylev <mundiir@gmail.com>
2018-04-11 16:41:14 +0300

    v2.2.18 (#27)
    ### Fixed
    - Incorrect processing of large files
    - Error handled trigger
    
    ### Changed 
    - Skipping files larger than 70 MB (Unicheck max file upload size)
    - Event observe validation

--- classes/services/storage/filesize_checker.class.php
+++ classes/services/storage/filesize_checker.class.php
@@ -14,80 +14,61 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
- * error_handled.php
+ * filesize_checker.class.php
  *
  * @package     plagiarism_unicheck
  * @subpackage  plagiarism
  * @author      Aleksandr Kostylev <a.kostylev@p1k.co.uk>
- * @copyright   2018 UKU Group, LTD, https://www.unicheck.com
+ * @copyright   UKU Group, LTD, https://www.unicheck.com
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace plagiarism_unicheck\event;
-
-use core\event\base;
-use plagiarism_unicheck;
+namespace plagiarism_unicheck\classes\services\storage;
 
 if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');
 }
 
-require_once(dirname(__FILE__) . '/../../locallib.php');
-
 /**
- * Class error_handled
+ * Class filesize_checker
  *
  * @package     plagiarism_unicheck
  * @subpackage  plagiarism
- *
  * @author      Aleksandr Kostylev <a.kostylev@p1k.co.uk>
  * @copyright   UKU Group, LTD, https://www.unicheck.com
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @since       Moodle 3.3
  */
-class error_handled extends base {
+class filesize_checker {
     /**
-     * Init method.
-     *
-     * @return void
+     * MAX_FILESIZE
      */
-    protected function init() {
-        $this->data['crud'] = 'r';
-        $this->data['edulevel'] = self::LEVEL_OTHER;
-        $this->context = \context_system::instance();
+    const MAX_FILESIZE = '70M';
+
+    /**
+     * Check if file is too large
+     *
+     * @param \stored_file $file
+     * @return bool
+     */
+    public static function file_is_to_large(\stored_file $file) {
+        if ($file->get_filesize() > get_real_size(self::MAX_FILESIZE)) {
+            return true;
+        }
+
+        return false;
     }
 
     /**
-     * Return the event name.
+     * Check if filesize is too large
      *
-     * @return string
+     * @param int $filesize In bytes
+     * @return bool
      */
-    public static function get_name() {
-        return plagiarism_unicheck::trans('event:error_handled');
-    }
+    public static function is_too_large($filesize) {
+        if ($filesize > get_real_size(self::MAX_FILESIZE)) {
+            return true;
+        }
 
-    /**
-     * Returns description of what happened.
-     *
-     * @return string
-     */
-    public function get_description() {
-        return "Error handled. Message '{$this->other['errormessage']}'";
-    }
-
-    /**
-     * Create from exception
-     *
-     * @param \Exception $exception
-     * @return base
-     */
-    public static function create_from_exception(\Exception $exception) {
-        $data = [
-            'other' => [
-                'errormessage' => $exception->getMessage(),
-            ]
-        ];
-
-        return self::create($data);
+        return false;
     }
 }
