710f1366ecce88e83e9f9de8b810fcdb9ab0f481
Aleksandr Kostylev <a.kostylev@p1k.co.uk>
2018-04-18 17:23:38 +0000

    add test autoload

--- tests/advanced_test.php
+++ tests/advanced_test.php
@@ -30,6 +30,7 @@ if (!defined('MOODLE_INTERNAL')) {
 global $CFG;
 
 require_once($CFG->dirroot . '/plagiarism/unicheck/tests/fixtures/unicheck_api_fixture.php');
+require_once($CFG->dirroot . '/plagiarism/unicheck/constants.php');
 require_once($CFG->dirroot . '/plagiarism/unicheck/autoloader.php');
 
 use plagiarism_unicheck_unittests\unicheck_api_fixture;
