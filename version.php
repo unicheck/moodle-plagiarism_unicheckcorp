c5987cba5e4bb1ffeeaca9f1f82ad0999362663c
Y2788496 <2788496@gmail.com>
2020-09-14 23:52:18 +0300

    Fix start scan online text (#95)

--- version.php
+++ version.php
@@ -28,12 +28,12 @@ if (!isset($plugin)) {
     $plugin = new stdClass();
 }
 
-$plugin->version = 2018021549; // YYYYMMDDVV.
+$plugin->version = 2018021550; // YYYYMMDDVV.
 $plugin->requires = 2017051501; // Requires Moodle 3.3 .
 $plugin->maturity = MATURITY_STABLE;
 
 $plugin->component = 'plagiarism_unicheck';
-$plugin->release = '2.7.0 (Build: 20200819)'; // Human-friendly version name.
+$plugin->release = '2.7.1 (Build: 20200819)'; // Human-friendly version name.
 $plugin->dependencies = [
     'mod_assign' => ANY_VERSION
 ];
