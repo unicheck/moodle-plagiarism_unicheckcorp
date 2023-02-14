92aee2c42b991ddf4a221f714b326da2bd92fa71
y.filchenko <y.filchenko@unicheck.com>
2023-02-14 09:51:07 +0200

    changed logo

--- version.php
+++ version.php
@@ -28,12 +28,12 @@ if (!isset($plugin)) {
     $plugin = new stdClass();
 }
 
-$plugin->version = 2020090108; // YYYYMMDDVV.
+$plugin->version = 2020090110; // YYYYMMDDVV.
 $plugin->requires = 2020061501; // Requires Moodle 3.9 .
 $plugin->maturity = MATURITY_STABLE;
 
 $plugin->component = 'plagiarism_unicheck';
-$plugin->release = '3.2.1 (Build: 20230116)'; // Human-friendly version name.
+$plugin->release = '3.2.2 (Build: 20230214)'; // Human-friendly version name.
 $plugin->dependencies = [
     'mod_assign' => ANY_VERSION
 ];
